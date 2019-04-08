2bb038bbb9939c8b3326ed3ed197636a8483704e
pedrooa <pedrooa@al.insper.edu.br>
2019-04-08 15:43:17 -0300

    Botao 3 simulando voltas

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -24,6 +24,11 @@ struct ili9488_opt_t g_ili9488_display_opt;
 #define LED_IDX_MASK  (1u << LED_IDX)
 
 
+#define BUT3_PIO           PIOA                  // periferico que controla o LED
+#define BUT3_PIO_ID        ID_PIOA                    // ID do periférico PIOC (controla LED)
+#define BUT3_PIO_IDX       19u                    // ID do LED no PIO
+#define BUT3_PIO_IDX_MASK  (1u << BUT3_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -34,7 +39,7 @@ struct ili9488_opt_t g_ili9488_display_opt;
 /* variaveis globais                                                    */
 /************************************************************************/
 volatile Bool f_rtt_alarme = false;
-
+volatile Bool but3_flag;
 
 
 /************************************************************************/
@@ -61,11 +66,38 @@ void RTT_Handler(void)
 	}
 }
 
+/**
+*  Handle Interrupcao botao 3
+*/
+static void Button3_Handler(uint32_t id, uint32_t mask)
+{
+	but3_flag = true;
+}
+
 
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
 
+
+/**
+* @Brief Inicializa o pino do BUT
+*/
+void BUT_init(void){
+	/* config. pino botao em modo de entrada */
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_FALL_EDGE, Button3_Handler);
+
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 1);
+};
 
 void pin_toggle(Pio *pio, uint32_t mask){
 	if(pio_get_output_data_status(pio, mask))
@@ -137,6 +169,7 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 
 
 int main(void) {
+	int ciclos = 0;
 	board_init();
 	sysclk_init();	
 	configure_lcd();
@@ -145,6 +178,50 @@ int main(void) {
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	font_draw_text(&arial_72, "102456", 50, 200, 2);
 	while(1) {
+		
+	if (but3_flag){
+		ciclos += 1;
+	}
+		
+	if (f_rtt_alarme){
+      
+      /*
+       * O clock base do RTT é 32678Hz
+       * Para gerar outra base de tempo é necessário
+       * usar o PLL pre scale, que divide o clock base.
+       *
+       * Nesse exemplo, estamos operando com um clock base
+       * de pllPreScale = 32768/32768/2 = 2Hz
+       *
+       * Quanto maior a frequência maior a resolução, porém
+       * menor o tempo máximo que conseguimos contar.
+       *
+       * Podemos configurar uma IRQ para acontecer quando 
+       * o contador do RTT atingir um determinado valor
+       * aqui usamos o irqRTTvalue para isso.
+       * 
+       * Nesse exemplo o irqRTTvalue = 8, causando uma
+       * interrupção a cada 4 segundos (lembre que usamos o 
+       * pllPreScale, cada incremento do RTT leva 500ms (2Hz).
+       */
+      uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
+      uint32_t irqRTTvalue  = 8;
+      
+      // reinicia RTT para gerar um novo IRQ
+      RTT_init(pllPreScale, irqRTTvalue);         
+      
+     /*
+      * caso queira ler o valor atual do RTT, basta usar a funcao
+      *   rtt_read_timer_value()
+      */
+      
+      /*
+       * CLEAR FLAG
+       */
+	  ciclos = 0;
+      f_rtt_alarme = false;
+    }
+  }
 		
 	}
 }
